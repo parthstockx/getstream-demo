57f179a0d612fa0112d7777a8d8b09ccd183bb3d
Parth_Chauhan <parthprafulbhaichauhan@stockx.com>
2025-11-24 16:53:14 +0530

    test

--- constant.ts
+++ constant.ts
@@ -10,10 +10,24 @@ const seller_user = {
   image: `https://getstream.io/random_png/?name=Seller`,
 };
 
+const buyer_user_2 = {
+  id: "buyer_2",
+  name: "Buyer 2",
+  image: `https://getstream.io/random_png/?name=Buyer-2`,
+};
+
+const seller_user_2 = {
+  id: "seller_2",
+  name: "Seller 2",
+  image: `https://getstream.io/random_png/?name=Seller-2`,
+};
+
 const getUser = (user_id: string) => {
   if (user_id === buyer_user.id) return buyer_user;
   if (user_id === seller_user.id) return seller_user;
+  if (user_id === buyer_user_2.id) return buyer_user_2;
+  if (user_id === seller_user_2.id) return seller_user_2;
   throw new Error("User not found");
 };
 
-export { getUser, buyer_user, seller_user };
+export { getUser, buyer_user, seller_user, buyer_user_2, seller_user_2 };
--- src/pages/api/create-channel.ts
+++ src/pages/api/create-channel.ts
@@ -1,23 +1,32 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getGetStreamInstance } from "../../../library/get-stream";
-import { buyer_user, getUser, seller_user } from "../../../constant";
+import {
+  buyer_user,
+  buyer_user_2,
+  seller_user,
+  seller_user_2,
+} from "../../../constant";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ message: string }>
 ) {
-  // const { query } = req;
+  const { query } = req;
 
-  // const channelName = query.name as string;
+  const channelName = query.name as string;
 
   const serverClient = getGetStreamInstance();
 
-  const channel = serverClient.channel("messaging", {
-    members: [buyer_user.id, seller_user.id],
-    created_by: getUser(buyer_user.id),
+  const channel = serverClient.channel("messaging", null, {
+    members: [buyer_user_2.id, seller_user_2.id],
+    created_by: buyer_user_2,
   });
 
   await channel.create();
+
+  await channel.updatePartial({
+    name: channelName,
+  });
 
   res.status(200).json({ message: "Channel created successfully" });
 }
